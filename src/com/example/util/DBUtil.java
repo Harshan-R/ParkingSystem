3ad49db2840829e8b1d893166872a5ecc69f5f7f
harshan <rhn0110@gmail.com>
2025-09-09 16:02:00 +0530

    Initial commit

---
+++ src/com/example/util/DBUtil.java
@@ -0,0 +1,14 @@
+package com.example.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+    private static final String URL = "jdbc:mysql://localhost:3306/parking_sys";
+    private static final String USER = "root";
+    private static final String PASSWORD = "newpassword";
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver"); 
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
